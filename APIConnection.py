6c80ba1905562e0dfe8fcfe52b1766da42883a65
m5preusser <124309201+m5preusser@users.noreply.github.com>
2024-01-26 12:38:39 -0700

    Update APIConnection.py

--- APIConnection.py
+++ APIConnection.py
@@ -3,9 +3,19 @@ import json, os
 
 class APIConnection:
 
-    def get_api_data(self):
+    # This 2D array contains all necessary metadata 
+    # Each row represents a the information for that day
+    # The first 3 rows have the following column structure:
+    # Day of week, Alpine danger value, Below treeline danger value, Treeline danger value
+    metadata = [
+        ["","","",""],
+        ["","","",""],
+        ["","","",""]
+    ]
 
-        print("Called get_api_data")
+    exposure = ""
+
+    def get_api_data(self):
 
         url = "https://api.avalanche.ca/forecasts/en/products/point?lat=50.60737&long=-115.12578"
 
@@ -13,7 +23,11 @@ class APIConnection:
 
         for key, value in response.json().items():
             if key == "report":
-                print("Found the report")
+                self.exposure = value["problems"][0]["factors"][1]["graphic"]["alt"]
                 dangerRatings = value["dangerRatings"]
                 for day in range(len(dangerRatings)):
-                    print(dangerRatings[day].type())
+                    self.metadata[day][0] = dangerRatings[day]["date"]["display"]
+                    self.metadata[day][1] = dangerRatings[day]["ratings"]["alp"]["rating"]["display"]
+                    self.metadata[day][2] = dangerRatings[day]["ratings"]["btl"]["rating"]["display"]
+                    self.metadata[day][3] = dangerRatings[day]["ratings"]["tln"]["rating"]["display"]
+                
